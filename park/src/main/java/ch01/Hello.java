e7961cee3ba61fbd6fd065c4dfc4b161641cdf0e
Jinsol01 <116528061+Jinsol01@users.noreply.github.com>
2022-11-29 14:44:10 +0900

    Second commit

--- park/src/main/java/ch01/Hello.java
+++ park/src/main/java/ch01/Hello.java
@@ -2,7 +2,10 @@ package ch01;
 
 public class Hello {
 	public static void main(String[] args) {
-	System.out.println("world01");
+	System.out.println("hello01");
+	System.out.println("hello02");
+	System.out.println("hello03");
+	
 }
 
 }
